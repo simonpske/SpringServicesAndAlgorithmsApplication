c18cc35076aa08384c1b266f9fa2811169e2403b
simonpske <philipp.simon.keller@gmail.com>
2025-03-22 20:59:57 +0100

    Adding first controller & Service

--- src/main/java/org/keller/learning/springservicesandalgorithms/service/BasicAlgorithmService.java
+++ src/main/java/org/keller/learning/springservicesandalgorithms/service/BasicAlgorithmService.java
@@ -8,10 +8,32 @@ import java.util.List;
 @Service
 public class BasicAlgorithmService {
 
-    public int getSearchAlgorithms() {
+    public int getSearchAlgorithms(String type) {
+
         int[] f = { 2, 4, 5, 6, 7, 8, 9, 11 };
         int k = 5;
-        return search(f,k);
+
+        if(type.equals("search")){
+            return search(f, k);
+        }
+        if(type.equals("binSearch")){
+            return binSearch(f, k);
+        }
+        return -1;
+    }
+
+    private int binSearch(int[] array, int key) {
+        int u = 0, o = array.length - 1;
+        while (u <= o) {
+            int m = (u + o) / 2;
+            if (array[m] == key)
+                return m;
+            else if (array[m] > key)
+                o = m - 1;
+            else
+                u = m + 1;
+        }
+        return -1;
     }
 
     private int search(int[] array, int key) {
