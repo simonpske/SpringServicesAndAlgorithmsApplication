7ab448a449bec7da2668b843638415b626db66a1
simonpske <philipp.simon.keller@gmail.com>
2025-03-22 14:41:37 +0100

    Initial Setup Structure

---
+++ src/main/java/org/keller/learning/springservicesandalgorithms/SpringServicesAndAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package org.keller.learning.springservicesandalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServicesAndAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServicesAndAlgorithmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/keller/learning/springservicesandalgorithms/SpringServicesAndAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.keller.learning.springservicesandalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServicesAndAlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
