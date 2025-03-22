5c259eaea2c0137b795fe2737d44b19da0940cd8
simonpske <philipp.simon.keller@gmail.com>
2025-03-22 20:43:52 +0100

    Adding first controller & Service

--- src/main/java/org/keller/learning/springservicesandalgorithms/controller/BasicAlgorithmController.java
+++ src/main/java/org/keller/learning/springservicesandalgorithms/controller/BasicAlgorithmController.java
@@ -1,5 +1,7 @@
 package org.keller.learning.springservicesandalgorithms.controller;
 
+import org.keller.learning.springservicesandalgorithms.service.BasicAlgorithmService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/algo/chapter1")
 public class BasicAlgorithmController {
+
+    @Autowired
+    BasicAlgorithmService basicAlgorithmService;
 
     @GetMapping("/sort")
     public String sort() {
