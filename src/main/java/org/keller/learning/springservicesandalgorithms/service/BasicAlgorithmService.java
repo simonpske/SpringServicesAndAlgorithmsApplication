59357af923c181888256e8cf8b9b697a2837d2fc
simonpske <philipp.simon.keller@gmail.com>
2025-03-22 20:51:48 +0100

    Adding first controller & Service

--- src/main/java/org/keller/learning/springservicesandalgorithms/service/BasicAlgorithmService.java
+++ src/main/java/org/keller/learning/springservicesandalgorithms/service/BasicAlgorithmService.java
@@ -2,6 +2,22 @@ package org.keller.learning.springservicesandalgorithms.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BasicAlgorithmService {
+
+    public int getSearchAlgorithms() {
+        int[] f = { 2, 4, 5, 6, 7, 8, 9, 11 };
+        int k = 5;
+        return search(f,k);
+    }
+
+    private int search(int[] array, int key) {
+        for (int i = 0; i < array.length; i++)
+            if (array[i] == key)
+                return i;
+        return -1;
+    }
 }
